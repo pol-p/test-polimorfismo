5dca82476d8805baeddd0412a855ae244b083713
Pol Puig <polpuigjuve@gmail.com>
2025-09-12 10:20:36 +0200

    feat: main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Shape[] v = new Shape[4];
+
+            v[0] = new Rectangle(5, 3);
+            v[1] = new Circle(5);
+            v[2] = new Square(5);
+            v[3] = new Circle(120);
+
+            double res = sum(v);
+
+            System.out.println("sum: " + res);
+
+            print(v);
+
+            sort(v);
+
+            print(v);
+        }
+        }
+    }
+}
